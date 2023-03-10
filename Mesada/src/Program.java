e3bc6f215651a305b3fbc19e3ac3e84636c495b2
Thiago <tw180896@gmail.com>
2023-03-10 13:47:32 -0400

    main

---
+++ Mesada/src/Program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner leitor = new Scanner(System.in);
+        int mesada = 50;
+        int entrada = leitor.nextInt();
+    }
+}
