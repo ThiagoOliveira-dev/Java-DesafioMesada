7714ff692bbd7890c21fd41fcc095a9b0749b9a0
Thiago <tw180896@gmail.com>
2023-03-10 14:05:44 -0400

    terminado

---
+++ Mesada/src/Calcula.java
@@ -0,0 +1,7 @@
+public class Calcula {
+    public static void calculaMes(int valor, int qtd){
+        int resultado;
+        resultado = valor * qtd ;
+        System.out.println( "Voce tera "+ resultado +" reais" );
+    }
+}
--- Mesada/src/Program.java
+++ Mesada/src/Program.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) throws Exception {
         
-        Scanner leitor = new Scanner(System.in);
-        int mesada = 50;
-        int entrada = leitor.nextInt();
+        try (Scanner leitor = new Scanner(System.in)) {
+            int mesada = 50;
+            int entrada = leitor.nextInt();
+
+      Calcula.calculaMes(mesada, entrada);
+        }
     }
 }
